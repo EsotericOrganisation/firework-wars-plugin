3e9dc8f015dbbb5c250559036c6038ce9d9a47c5
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-08-08 15:36:05 +0100

    Improve config options

--- src/main/java/net/slqmy/firework_wars_plugin/arena/Arena.java
+++ src/main/java/net/slqmy/firework_wars_plugin/arena/Arena.java
@@ -14,9 +14,9 @@ public class Arena {
   @Expose
   private PlayerLocation lobbySpawnLocation;
   @Expose
-  private @Nullable int minimumPlayerCount;
+  private @Nullable int minimumPlayerCount = 2;
   @Expose
-  private @Nullable int maximumPlayerCount;
+  private @Nullable int maximumPlayerCount = PlayerCount.INFINITY.getValue();
   @Expose
   private int countDownSeconds = 15;
   @Expose
@@ -46,5 +46,19 @@ public class Arena {
 
   public BlockLocation[] getChestLocations() {
     return chestLocations;
+  }
+
+  public enum PlayerCount {
+    INFINITY(-1);
+
+    private int value;
+
+    public int getValue() {
+      return value;
+    }
+
+    PlayerCount(int value) {
+      this.value = value;
+    }
   }
 }
--- src/main/java/net/slqmy/firework_wars_plugin/arena/ConfiguredTeam.java
+++ src/main/java/net/slqmy/firework_wars_plugin/arena/ConfiguredTeam.java
@@ -1,7 +1,8 @@
 package net.slqmy.firework_wars_plugin.arena;
 
 public class ConfiguredTeam {
-  
+
+  private String miniMessageString;
   private PlayerLocation spawnLocation;
 
 }
