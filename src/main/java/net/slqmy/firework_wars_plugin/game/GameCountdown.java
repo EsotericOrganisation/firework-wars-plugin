553c8eb9bb68e36b737bac31ffbe668ca40351fe
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-08-08 15:10:01 +0100

    Differentiate between singular and pluaral countdown seconds

--- src/main/java/net/slqmy/firework_wars_plugin/game/GameCountdown.java
+++ src/main/java/net/slqmy/firework_wars_plugin/game/GameCountdown.java
@@ -30,7 +30,7 @@ public class GameCountdown extends BukkitRunnable {
 
   @Override
   public void run() {
-    game.sendMessage(Message.GAME_STARTING_IN_TIME, countDownSeconds);
+    game.sendMessage(countDownSeconds == 1 ? Message.GAME_STARTING_IN_TIME_SINGULAR : Message.GAME_STARTING_IN_TIME_PLUARAL, countDownSeconds);
 
     countDownSeconds--;
     if (countDownSeconds == 0) {
