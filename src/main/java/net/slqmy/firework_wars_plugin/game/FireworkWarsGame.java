6d445dc0803e1bef47cd21680b07ff14a6e6f581
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-08-08 01:31:38 +0100

    Add class to represent a game

---
+++ src/main/java/net/slqmy/firework_wars_plugin/game/FireworkWarsGame.java
@@ -0,0 +1,20 @@
+package net.slqmy.firework_wars_plugin.game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.entity.Player;
+
+import net.slqmy.firework_wars_plugin.arena.Arena;
+
+public class FireworkWarsGame {
+  
+  private Arena arena;
+
+  private List<Player> players = new ArrayList<>();
+
+  public void addPlayer(Player player) {
+    players.add(player);
+    player.teleport(arena.getLobbySpawnLocation());
+  }
+}
