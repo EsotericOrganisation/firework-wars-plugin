67e8dc003dba5e783e23abce7683a4ff613f59ab
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-08-08 16:04:16 +0100

    Use annotation

--- src/main/java/net/slqmy/firework_wars_plugin/arena/ConfiguredTeam.java
+++ src/main/java/net/slqmy/firework_wars_plugin/arena/ConfiguredTeam.java
@@ -1,8 +1,12 @@
 package net.slqmy.firework_wars_plugin.arena;
 
+import com.google.gson.annotations.Expose;
+
 public class ConfiguredTeam {
 
+  @Expose
   private String miniMessageString;
+  @Expose
   private PlayerLocation spawnLocation;
 
   public String getMiniMessageString() {
