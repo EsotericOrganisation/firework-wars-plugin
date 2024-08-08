6471c5cb7c909f94ba70ca7cf1e70c697871d20c
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-08-08 01:13:09 +0100

    Add lobby spawn location field

--- src/main/java/net/slqmy/firework_wars_plugin/arena/Arena.java
+++ src/main/java/net/slqmy/firework_wars_plugin/arena/Arena.java
@@ -1,7 +1,10 @@
 package net.slqmy.firework_wars_plugin.arena;
 
+import org.bukkit.Location;
+
 import net.slqmy.firework_wars_plugin.arena.BlockLocation;
 
 public class Arena {
+  private Location lobbySpawnLocation;
   private BlockLocation[] blockLocations;
 }
