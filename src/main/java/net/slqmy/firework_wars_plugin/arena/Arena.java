842b041fb0a2a5d0b6ac5483043e9324d6d98d5f
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-08-08 01:02:00 +0100

    Use arrays instead of lists

--- src/main/java/net/slqmy/firework_wars_plugin/arena/Arena.java
+++ src/main/java/net/slqmy/firework_wars_plugin/arena/Arena.java
@@ -2,8 +2,6 @@ package net.slqmy.firework_wars_plugin.arena;
 
 import net.slqmy.firework_wars_plugin.arena.BlockLocation;
 
-import java.util.List;
-
 public class Arena {
-  private List<BlockLocation> blockLocations;
+  private BlockLocation[] blockLocations;
 }
--- src/main/java/net/slqmy/firework_wars_plugin/arena/ArenaInformation.java
+++ src/main/java/net/slqmy/firework_wars_plugin/arena/ArenaInformation.java
@@ -2,8 +2,6 @@ package net.slqmy.firework_wars_plugin.arena;
 
 import net.slqmy.firework_wars_plugin.arena.Arena;
 
-import java.util.List;
-
 public class ArenaInformation {
-  private List<Arena> arenas;
+  private Arena[] arenas;
 }
