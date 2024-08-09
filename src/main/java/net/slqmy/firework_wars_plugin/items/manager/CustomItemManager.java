2480cc86a639200da36c1638cc04a75ff4beee82
rolyPolyVole <yezhanting@gmail.com>
2024-08-10 00:37:27 +0700

    Use reflection to unfreeze the built-in item registry to prevent startup error.

--- src/main/java/net/slqmy/firework_wars_plugin/items/manager/CustomItemManager.java
+++ src/main/java/net/slqmy/firework_wars_plugin/items/manager/CustomItemManager.java
@@ -1,5 +1,8 @@
 package net.slqmy.firework_wars_plugin.items.manager;
 
+import net.minecraft.core.DefaultedMappedRegistry;
+import net.minecraft.core.MappedRegistry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.slqmy.firework_wars_plugin.FireworkWarsPlugin;
@@ -7,20 +10,32 @@ import net.slqmy.firework_wars_plugin.items.FireworkRifleItem;
 import net.slqmy.firework_wars_plugin.items.RifleAmmo;
 import net.slqmy.firework_wars_plugin.items.nms.CustomCrossbow;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.util.HashMap;
+import java.util.IdentityHashMap;
 import java.util.Map;
 
 public class CustomItemManager {
+  private final FireworkWarsPlugin plugin;
+
   private final Map<String, AbstractItem> itemRegistry = new HashMap<>();
   private final Map<String, Item> nmsItemRegistry = new HashMap<>();
 
   public CustomItemManager(FireworkWarsPlugin plugin) {
+    this.plugin = plugin;
+
     registerItem(new FireworkRifleItem(plugin));
     registerItem(new RifleAmmo(plugin));
+
+    reopenItemRegistry();
 
     registerNMSItem(
         "firework_rifle_crossbow",
         new CustomCrossbow(CustomCrossbow.PROPERTIES, getItem("firework_rifle_ammo")));
+
+    BuiltInRegistries.ITEM.freeze();
   }
 
   public Map<String, AbstractItem> getItemRegistry() {
@@ -46,5 +61,20 @@ public class CustomItemManager {
   private void registerNMSItem(String id, Item item) {
     Items.registerItem(id, item);
     nmsItemRegistry.put(id, item);
+  }
+
+  private void reopenItemRegistry() {
+    try {
+      Field frozenField = MappedRegistry.class.getDeclaredField("frozen");
+      Field unregisteredHoldersField = MappedRegistry.class.getDeclaredField("unregisteredIntrusiveHolders");
+
+      frozenField.setAccessible(true);
+      unregisteredHoldersField.setAccessible(true);
+
+      frozenField.setBoolean(BuiltInRegistries.ITEM, false);
+      unregisteredHoldersField.set(BuiltInRegistries.ITEM, new IdentityHashMap<>());
+    } catch (IllegalAccessException | NoSuchFieldException e) {
+      plugin.getLogger().severe("Failed to reopen the item registry! " + e.getMessage());
+    }
   }
 }
