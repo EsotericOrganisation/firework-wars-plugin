d218e7ba8a1dc4fb03fec623d9240c6256d0c30b
rolyPolyVole <yezhanting@gmail.com>
2024-10-03 23:54:58 +0100

    Use pdc manager (pdc manager >>>), remove redundant methods in firework explode event listener, decrease explosion power slightly

--- src/main/java/org/esoteric_organisation/firework_wars_plugin/items/guns/rocket_launcher/RocketLauncherItem.java
+++ src/main/java/org/esoteric_organisation/firework_wars_plugin/items/guns/rocket_launcher/RocketLauncherItem.java
@@ -1,24 +1,18 @@
 package org.esoteric_organisation.firework_wars_plugin.items.guns.rocket_launcher;
 
 import io.papermc.paper.event.entity.EntityLoadCrossbowEvent;
-import org.bukkit.Material;
 import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.event.entity.FireworkExplodeEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.persistence.PersistentDataContainer;
-import org.bukkit.persistence.PersistentDataType;
 import org.esoteric_organisation.firework_wars_plugin.FireworkWarsPlugin;
 import org.esoteric_organisation.firework_wars_plugin.game.FireworkWarsGame;
 import org.esoteric_organisation.firework_wars_plugin.items.guns.BaseGunItem;
 import org.esoteric_organisation.firework_wars_plugin.language.Message;
 import org.esoteric_organisation.firework_wars_plugin.util.Keys;
-import org.esoteric_organisation.firework_wars_plugin.util.PersistentDataManager;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.List;
 
 public class RocketLauncherItem extends BaseGunItem {
 
@@ -33,40 +27,28 @@ public class RocketLauncherItem extends BaseGunItem {
 
   @Override
   protected void onCrossbowShoot(Player player, FireworkWarsGame game, EntityShootBowEvent event) {
-    PersistentDataContainer pdc = event.getProjectile().getPersistentDataContainer();
-
-    pdc.set(Keys.EXPLOSIVE_ROCKET_ENTITY, PersistentDataType.BOOLEAN, true);
+    pdcManager.setBooleanValue(
+      event.getProjectile(), Keys.EXPLOSIVE_ROCKET_ENTITY, true);
   }
 
   @Override
   public ItemStack getItem(Player player) {
-    ItemStack rocketLauncher = new ItemStack(Material.CROSSBOW);
-
-    rocketLauncher.editMeta(
-      (meta) -> {
-        meta.itemName(
-          plugin.getLanguageManager().getMessage(Message.ROCKET_LAUNCHER, player)
-        );
-
-        meta.lore(List.of(plugin.getLanguageManager().getMessage(Message.ROCKET_LAUNCHER_LORE, player)));
-      }
-    );
-
-    return rocketLauncher;
+    return getBaseCrossbowBuilder()
+      .setName(Message.ROCKET_LAUNCHER, player)
+      .setLore(Message.ROCKET_LAUNCHER_LORE, player)
+      .build();
   }
 
   @EventHandler
   public void onFireworkExplode(@NotNull FireworkExplodeEvent event) {
     Firework firework = event.getEntity();
-
-    Boolean isRocket = firework.getPersistentDataContainer().get(Keys.EXPLOSIVE_ROCKET_ENTITY, PersistentDataType.BOOLEAN);
+    Boolean isRocket = pdcManager.getBooleanValue(firework, Keys.EXPLOSIVE_ROCKET_ENTITY);
 
     if (isRocket != null && isRocket) {
+      firework.getWorld().createExplosion(
+        firework, firework.getLocation(), 3.0F, false, true);
+
       event.setCancelled(true);
-
-      firework.getWorld().createExplosion(firework, firework.getLocation(), 3.5F, false, true);
-
-      firework.remove();
     }
   }
 }
