8575d92b7d326af3167b396046a7aba30f21cc04
rolyPolyVole <yezhanting@gmail.com>
2024-10-04 21:14:32 +0100

    Override modify meta method rather than attaching another consumer

--- src/main/java/org/esoteric_organisation/firework_wars_plugin/items/guns/rifle/FireworkRifleItem.java
+++ src/main/java/org/esoteric_organisation/firework_wars_plugin/items/guns/rifle/FireworkRifleItem.java
@@ -2,19 +2,18 @@ package org.esoteric_organisation.firework_wars_plugin.items.guns.rifle;
 
 import io.papermc.paper.event.entity.EntityLoadCrossbowEvent;
 import org.bukkit.Bukkit;
-import org.esoteric_organisation.firework_wars_plugin.FireworkWarsPlugin;
-import org.esoteric_organisation.firework_wars_plugin.game.FireworkWarsGame;
-import org.esoteric_organisation.firework_wars_plugin.game.FireworkWarsTeam;
-import org.esoteric_organisation.firework_wars_plugin.items.guns.BaseGunItem;
-import org.esoteric_organisation.firework_wars_plugin.language.Message;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.CrossbowMeta;
+import org.esoteric_organisation.firework_wars_plugin.FireworkWarsPlugin;
+import org.esoteric_organisation.firework_wars_plugin.game.FireworkWarsGame;
+import org.esoteric_organisation.firework_wars_plugin.game.FireworkWarsTeam;
+import org.esoteric_organisation.firework_wars_plugin.items.guns.BaseGunItem;
+import org.esoteric_organisation.firework_wars_plugin.language.Message;
 
 import java.util.List;
-import java.util.function.Consumer;
 
 public class FireworkRifleItem extends BaseGunItem {
 
@@ -24,15 +23,16 @@ public class FireworkRifleItem extends BaseGunItem {
 
   @Override
   public ItemStack getItem(Player player) {
-    Consumer<CrossbowMeta> consumer = this::modifyMeta;
-    Consumer<CrossbowMeta> modifyMeta = consumer.andThen(meta ->
-      meta.addEnchant(Enchantment.QUICK_CHARGE, 3, true));
-
     return getBaseCrossbowBuilder()
       .setName(Message.FIREWORK_RIFLE, player)
       .setLore(Message.FIREWORK_RIFLE_LORE, player)
-      .modifyMeta(modifyMeta)
       .build();
+  }
+
+  @Override
+  protected void modifyMeta(CrossbowMeta meta) {
+    super.modifyMeta(meta);
+    meta.addEnchant(Enchantment.QUICK_CHARGE, 3, true);
   }
 
   @Override
