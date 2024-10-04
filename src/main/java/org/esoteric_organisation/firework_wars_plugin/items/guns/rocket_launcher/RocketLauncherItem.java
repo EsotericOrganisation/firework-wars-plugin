a5813549726b446da1f0881fc4bdc696ffb60544
rolyPolyVole <yezhanting@gmail.com>
2024-10-04 21:15:48 +0100

    Add a firework star on crossbow load to trigger explosions upon block collision, fix infinite explosion glitch

--- src/main/java/org/esoteric_organisation/firework_wars_plugin/items/guns/rocket_launcher/RocketLauncherItem.java
+++ src/main/java/org/esoteric_organisation/firework_wars_plugin/items/guns/rocket_launcher/RocketLauncherItem.java
@@ -1,18 +1,22 @@
 package org.esoteric_organisation.firework_wars_plugin.items.guns.rocket_launcher;
 
 import io.papermc.paper.event.entity.EntityLoadCrossbowEvent;
+import org.bukkit.Color;
 import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.event.entity.FireworkExplodeEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.CrossbowMeta;
 import org.esoteric_organisation.firework_wars_plugin.FireworkWarsPlugin;
 import org.esoteric_organisation.firework_wars_plugin.game.FireworkWarsGame;
 import org.esoteric_organisation.firework_wars_plugin.items.guns.BaseGunItem;
 import org.esoteric_organisation.firework_wars_plugin.language.Message;
 import org.esoteric_organisation.firework_wars_plugin.util.Keys;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class RocketLauncherItem extends BaseGunItem {
 
@@ -22,7 +26,8 @@ public class RocketLauncherItem extends BaseGunItem {
 
   @Override
   protected void onCrossbowLoad(Player player, FireworkWarsGame game, EntityLoadCrossbowEvent event) {
-
+    plugin.getServer().getScheduler().runTaskLater(plugin, () ->
+      event.getCrossbow().editMeta((meta) -> ((CrossbowMeta) meta).setChargedProjectiles(List.of(createFirework(Color.RED, 1)))), 1L);
   }
 
   @Override
@@ -46,8 +51,9 @@ public class RocketLauncherItem extends BaseGunItem {
 
     if (isRocket != null && isRocket) {
       firework.getWorld().createExplosion(
-        firework, firework.getLocation(), 3.0F, false, true);
+        firework, firework.getLocation(), 2.5F, false, true);
 
+      firework.remove();
       event.setCancelled(true);
     }
   }
