c7c1f6d54d9f10bf4523fb20ed02f085cc6ff8d7
rolyPolyVole <yezhanting@gmail.com>
2024-10-08 01:39:39 +0100

    Add new methods, remove static unregisterGame() as it was unnecessary

--- src/main/java/org/esoteric_organisation/firework_wars_plugin/game/team/TeamPlayer.java
+++ src/main/java/org/esoteric_organisation/firework_wars_plugin/game/team/TeamPlayer.java
@@ -2,7 +2,10 @@ package org.esoteric_organisation.firework_wars_plugin.game.team;
 
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.esoteric_organisation.firework_wars_plugin.FireworkWarsPlugin;
 import org.esoteric_organisation.firework_wars_plugin.game.FireworkWarsGame;
 
 import java.util.HashMap;
@@ -10,71 +13,82 @@ import java.util.Map;
 import java.util.UUID;
 
 public class TeamPlayer {
-    private final static Map<UUID, TeamPlayer> activePlayers = new HashMap<>();
+  private final static Map<UUID, TeamPlayer> activePlayers = new HashMap<>();
 
-    private final UUID uuid;
-    private final FireworkWarsGame game;
+  private final UUID uuid;
+  private final FireworkWarsGame game;
+  private final FireworkWarsPlugin plugin;
 
-    private FireworkWarsTeam team;
+  private FireworkWarsTeam team;
 
-    public static TeamPlayer from(UUID uuid) {
-        return activePlayers.get(uuid);
-    }
+  public static TeamPlayer from(UUID uuid) {
+    return activePlayers.get(uuid);
+  }
 
-    public static TeamPlayer from(Player player) {
-        return from(player.getUniqueId());
-    }
+  public static TeamPlayer from(Player player) {
+    return from(player.getUniqueId());
+  }
 
-    public static void unregisterGame(FireworkWarsGame game) {
-        game.getPlayers().forEach(player -> player.unregister(false));
-    }
+  public TeamPlayer(UUID uuid, FireworkWarsGame game) {
+    this.uuid = uuid;
+    this.game = game;
+    this.plugin = game.getPlugin();
 
-    public TeamPlayer(UUID uuid, FireworkWarsGame game) {
-        this.uuid = uuid;
-        this.game = game;
+    register();
+  }
 
-        register();
-    }
-
-    private void register() {
-        activePlayers.forEach((uuid, player) -> {
-            if (uuid.equals(this.uuid)) {
-                player.unregister(true);
-            }
-        });
-
-        activePlayers.put(uuid, this);
-    }
-
-    public void unregister(boolean removeFromGame) {
-        activePlayers.remove(uuid);
-
-        if (removeFromGame) {
-          game.getPlayers().remove(this);
+  private void register() {
+    activePlayers.forEach((uuid, player) -> {
+        if (uuid.equals(this.uuid)) {
+            player.unregister(true);
         }
-    }
+    });
 
-    public void setTeam(FireworkWarsTeam team) {
-        this.team = team;
-    }
+    activePlayers.put(uuid, this);
+  }
 
-    public FireworkWarsTeam getTeam() {
-        return team;
-    }
+  public void unregister(boolean removeFromGame) {
+    activePlayers.remove(uuid);
 
-    public UUID getUuid() {
-        return uuid;
+    if (removeFromGame) {
+      game.getPlayers().remove(this);
     }
+  }
 
-    public Player getPlayer() {
-        return Bukkit.getOfflinePlayer(uuid).getPlayer();
-    }
+  public void setTeam(FireworkWarsTeam team) {
+    this.team = team;
+  }
 
-    public Component getColoredName() {
-        return getPlayer().displayName().color(team.getTeamColor());
-    }
+  public FireworkWarsTeam getTeam() {
+    return team;
+  }
 
-    public void sendMessage(Component message) {
-        getPlayer().sendMessage(message);
-    }
+  public UUID getUuid() {
+    return uuid;
+  }
+
+  public Player getPlayer() {
+    return Bukkit.getOfflinePlayer(uuid).getPlayer();
+  }
+
+  public Component getColoredName() {
+    return getPlayer().displayName().color(team.getTeamColor());
+  }
+
+  public void sendMessage(Component message) {
+    getPlayer().sendMessage(message);
+  }
+
+  public void teleportToWaitingArea() {
+    getPlayer().teleport(game.getArena().getWaitingAreaLocation().getBukkitLocation());
+  }
+
+  public void teleportToLobby() {
+    Location location = plugin.getArenaManager().getLobbies().get(0).getSpawnLocation().getBukkitLocation();
+    getPlayer().teleport(location);
+  }
+
+  public void becomeSpectator() {
+    getPlayer().setGameMode(GameMode.SPECTATOR);
+  }
 }
