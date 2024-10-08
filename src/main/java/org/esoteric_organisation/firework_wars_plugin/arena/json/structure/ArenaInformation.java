ed1848eb95955fc2e557bd8166a9c38faaa5b6f8
rolyPolyVole <yezhanting@gmail.com>
2024-10-08 01:21:26 +0100

    Make getters return immutable lists instead of arrays

---
+++ src/main/java/org/esoteric_organisation/firework_wars_plugin/arena/json/structure/ArenaInformation.java
@@ -0,0 +1,20 @@
+package org.esoteric_organisation.firework_wars_plugin.arena.json.structure;
+
+import org.esoteric_organisation.firework_wars_plugin.arena.json.data_holder.Arena;
+import org.esoteric_organisation.firework_wars_plugin.arena.json.data_holder.Lobby;
+
+import java.util.List;
+
+@SuppressWarnings("unused")
+public class ArenaInformation {
+  private Lobby[] lobbies;
+  private Arena[] arenas;
+
+  public List<Lobby> getLobbies() {
+    return lobbies == null ? null : List.of(lobbies);
+  }
+
+  public List<Arena> getArenas() {
+    return arenas == null ? null : List.of(arenas);
+  }
+}
