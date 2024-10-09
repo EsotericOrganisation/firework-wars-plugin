dcf53cf538668eaaf407523603f2449935e1c34a
rolyPolyVole <yezhanting@gmail.com>
2024-10-09 20:23:35 +0100

    Add randomInt(min, max) method

--- src/main/java/org/esoteric_organisation/firework_wars_plugin/util/Util.java
+++ src/main/java/org/esoteric_organisation/firework_wars_plugin/util/Util.java
@@ -24,4 +24,8 @@ public class Util {
             .filter(Objects::nonNull)
             .anyMatch(predicate);
     }
+
+    public static int randomInt(int min, int max) {
+        return (int) (Math.random() * (max - min + 1) + min);
+    }
 }
