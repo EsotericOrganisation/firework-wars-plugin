a7d4411aa8485192b61ea95a20c9690652d65844
rolyPolyVole <yezhanting@gmail.com>
2024-10-09 20:20:51 +0100

    Move addPlayer() logic to the TeamPlayer file

--- src/main/java/org/esoteric_organisation/firework_wars_plugin/game/team/FireworkWarsTeam.java
+++ src/main/java/org/esoteric_organisation/firework_wars_plugin/game/team/FireworkWarsTeam.java
@@ -3,11 +3,8 @@ package org.esoteric_organisation.firework_wars_plugin.game.team;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.minimessage.MiniMessage;
-import net.kyori.adventure.title.TitlePart;
-import org.bukkit.entity.Player;
 import org.esoteric_organisation.firework_wars_plugin.FireworkWarsPlugin;
 import org.esoteric_organisation.firework_wars_plugin.arena.json.data_holder.TeamData;
-import org.esoteric_organisation.firework_wars_plugin.language.Message;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,13 +32,6 @@ public class FireworkWarsTeam {
 
     public void addPlayer(TeamPlayer teamPlayer) {
         players.add(teamPlayer);
-        teamPlayer.setTeam(this);
-
-        Player player = teamPlayer.getPlayer();
-        player.teleport(teamData.getSpawnLocation().getBukkitLocation());
-
-        player.sendTitlePart(TitlePart.TITLE, plugin.getLanguageManager().getMessage(Message.YOU_ARE_ON_TEAM, player));
-        player.sendTitlePart(TitlePart.SUBTITLE, getColoredTeamName());
     }
 
     public Component getColoredTeamName() {
