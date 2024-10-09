7b0ab55bc2c0bdb633a8f7f28e6ab8d813092c21
rolyPolyVole <yezhanting@gmail.com>
2024-10-09 20:21:17 +0100

    Add damage event, added logic to update scoreboards on kill/damage

--- src/main/java/org/esoteric_organisation/firework_wars_plugin/event/listeners/GameEventListener.java
+++ src/main/java/org/esoteric_organisation/firework_wars_plugin/event/listeners/GameEventListener.java
@@ -1,21 +1,31 @@
 package org.esoteric_organisation.firework_wars_plugin.event.listeners;
 
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.esoteric_organisation.firework_wars_plugin.FireworkWarsPlugin;
 import org.esoteric_organisation.firework_wars_plugin.game.FireworkWarsGame;
+import org.esoteric_organisation.firework_wars_plugin.game.team.FireworkWarsTeam;
+import org.esoteric_organisation.firework_wars_plugin.game.team.TeamPlayer;
+import org.esoteric_organisation.firework_wars_plugin.util.ReflectUtil;
+
+import java.util.List;
 
 public class GameEventListener implements Listener {
-
     private final FireworkWarsPlugin plugin;
     private final FireworkWarsGame game;
+
+    private final ReflectUtil reflectUtil;
 
     public GameEventListener(FireworkWarsPlugin plugin, FireworkWarsGame game) {
         this.plugin = plugin;
         this.game = game;
+
+        this.reflectUtil = new ReflectUtil();
     }
 
     public void register() {
@@ -27,6 +37,24 @@ public class GameEventListener implements Listener {
     }
 
     @EventHandler
+    @SuppressWarnings("UnstableApiUsage")
+    public void onPlayerDamage(EntityDamageByEntityEvent event) {
+        if (!(event.getEntity() instanceof Player player)) {
+            return;
+        }
+
+        if (!(event.getDamageSource().getCausingEntity() instanceof Player damager)) {
+            return;
+        }
+
+        if (!game.isAlive(player) || !game.isAlive(damager)) {
+            return;
+        }
+
+        TeamPlayer.from(damager).addDamage((int) Math.round(event.getFinalDamage()));
+    }
+
+    @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
         Player player = event.getPlayer();
 
@@ -34,6 +62,22 @@ public class GameEventListener implements Listener {
             return;
         }
 
-        game.onPlayerDeath(event);
+        player.setGameMode(GameMode.SPECTATOR);
+        plugin.runTaskLater(() -> player.spigot().respawn(), 1L);
+
+        if (player.getKiller() != null) {
+            TeamPlayer.from(player.getKiller()).incrementKills();
+        }
+
+        FireworkWarsTeam team = TeamPlayer.from(player.getUniqueId()).getTeam();
+
+        if (game.isTeamEliminated(team)) {
+            game.eliminateTeam(team);
+            List<FireworkWarsTeam> remainingTeams = game.getRemainingTeams();
+
+            if (remainingTeams.size() == 1) {
+                game.preEndGame(remainingTeams.get(0));
+            }
+        }
     }
 }
