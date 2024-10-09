3d65f2cda5a98de982f1b965c62d4f27c8add2c3
rolyPolyVole <yezhanting@gmail.com>
2024-10-09 20:17:27 +0100

    Add data holder class to read endgame config

---
+++ src/main/java/org/esoteric_organisation/firework_wars_plugin/arena/json/data_holder/EndgameData.java
@@ -0,0 +1,42 @@
+package org.esoteric_organisation.firework_wars_plugin.arena.json.data_holder;
+
+import org.esoteric_organisation.firework_wars_plugin.arena.json.mini_components.PlayerLocation;
+
+@SuppressWarnings("unused")
+public class EndgameData {
+    private double endgameStartMinutes;
+
+    private int warnBeforeEndgameSeconds;
+    private int warnBeforeGameEndSeconds;
+
+    private PlayerLocation wardenSpawnLocation;
+
+
+    public double getEndgameStartMinutes() {
+        return endgameStartMinutes;
+    }
+
+    public int getEndgameStartTicks() {
+        return (int) (endgameStartMinutes * 60 * 20);
+    }
+
+    public int getWarnBeforeEndgameSeconds() {
+        return warnBeforeEndgameSeconds;
+    }
+
+    public int getWarnBeforeEndgameTicks() {
+        return warnBeforeEndgameSeconds * 20;
+    }
+
+    public int getWarnBeforeGameEndSeconds() {
+        return warnBeforeGameEndSeconds;
+    }
+
+    public int getWarnBeforeGameEndTicks() {
+        return warnBeforeGameEndSeconds * 20;
+    }
+
+    public PlayerLocation getWardenSpawnLocation() {
+        return wardenSpawnLocation;
+    }
+}
