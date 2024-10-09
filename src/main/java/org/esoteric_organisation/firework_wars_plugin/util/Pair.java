90e87b573d79e651283f09bc2406abb6d3643698
rolyPolyVole <yezhanting@gmail.com>
2024-10-09 20:23:15 +0100

    Add pair class, used in updating scoreboards

---
+++ src/main/java/org/esoteric_organisation/firework_wars_plugin/util/Pair.java
@@ -0,0 +1,23 @@
+package org.esoteric_organisation.firework_wars_plugin.util;
+
+public class Pair<A, B> {
+    private final A a;
+    private final B b;
+
+    public static <A, B> Pair<A, B> of(A a, B b) {
+        return new Pair<>(a, b);
+    }
+
+    public Pair(A a, B b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public A getLeft() {
+        return a;
+    }
+
+    public B getRight() {
+        return b;
+    }
+}
