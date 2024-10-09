8b3582d62380128a83e0f519f592e8744cf0e05b
rolyPolyVole <yezhanting@gmail.com>
2024-10-09 20:14:57 +0100

    Add supply drop data, endgame data and more fields & methods

--- src/main/java/org/esoteric_organisation/firework_wars_plugin/arena/json/structure/Arena.java
+++ src/main/java/org/esoteric_organisation/firework_wars_plugin/arena/json/structure/Arena.java
@@ -1,5 +1,8 @@
-package org.esoteric_organisation.firework_wars_plugin.arena.json.data_holder;
+package org.esoteric_organisation.firework_wars_plugin.arena.json.structure;
 
+import org.esoteric_organisation.firework_wars_plugin.arena.json.data_holder.EndgameData;
+import org.esoteric_organisation.firework_wars_plugin.arena.json.data_holder.SupplyDropData;
+import org.esoteric_organisation.firework_wars_plugin.arena.json.data_holder.TeamData;
 import org.esoteric_organisation.firework_wars_plugin.arena.json.mini_components.BlockLocation;
 import org.esoteric_organisation.firework_wars_plugin.arena.json.mini_components.PlayerLocation;
 
@@ -15,10 +18,15 @@ public class Arena {
     private int maximumPlayerCount = PlayerCount.INFINITY.getValue();
 
     private int countDownSeconds = 15;
+    private double gameDurationMinutes = 30;
 
     private TeamData[] teamInformation;
 
     private BlockLocation[] chestLocations;
+
+    private SupplyDropData supplyDropData;
+
+    private EndgameData endgameData;
 
     public List<String> getWorlds() {
         return List.of(worlds);
@@ -40,12 +48,32 @@ public class Arena {
         return countDownSeconds;
     }
 
+    public double getGameDurationMinutes() {
+        return gameDurationMinutes;
+    }
+
+    public int getGameDurationTicks() {
+        return (int) (gameDurationMinutes * 60 * 20);
+    }
+
     public List<TeamData> getTeamInformation() {
         return List.of(teamInformation);
     }
 
     public List<BlockLocation> getChestLocations() {
         return List.of(chestLocations);
+    }
+
+    public SupplyDropData getSupplyDropData() {
+        return supplyDropData;
+    }
+
+    public EndgameData getEndgameData() {
+        return endgameData;
+    }
+
+    public double getEndgameDurationTicks() {
+        return getGameDurationTicks() - endgameData.getEndgameStartTicks();
     }
 
     public enum PlayerCount {
