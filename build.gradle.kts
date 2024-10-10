75b1790b31760808d2f276f52759a29ee8ea27d4
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-10-10 18:09:55 +0100

    Add needed dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,6 +75,7 @@ dependencies {
 
   implementation("dev.jorel" , "commandapi-bukkit-shade-mojang-mapped" , "9.5.1")
   implementation("org.apache.commons", "commons-compress", "1.24.0")
+  implementation("org.tukaani", "xz", "1.9")
 }
 
 tasks {
