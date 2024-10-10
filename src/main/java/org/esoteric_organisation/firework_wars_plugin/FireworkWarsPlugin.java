1c55946858e7e6e76b1daeff4fd94aea1b97ca87
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-10-10 18:30:55 +0100

    Save map to root directory

--- src/main/java/org/esoteric_organisation/firework_wars_plugin/FireworkWarsPlugin.java
+++ src/main/java/org/esoteric_organisation/firework_wars_plugin/FireworkWarsPlugin.java
@@ -104,7 +104,7 @@ public final class FireworkWarsPlugin extends JavaPlugin {
 
     private void saveMaps() throws IOException {
         saveResource("maps/barracks/barracks.7z", false);
-        FileUtil.extract7z("plugins/" + getName() + "/maps/barracks/barracks.7z", "maps/barracks");
+        FileUtil.extract7z("plugins/" + getName() + "/maps/barracks/barracks.7z", "barracks");
     }
 
     public void runTaskLater(Runnable runnable, long delay) {
