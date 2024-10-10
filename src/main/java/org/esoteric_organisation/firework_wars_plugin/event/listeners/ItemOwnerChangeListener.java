7ffde20c93c2fa102c933f29e4e7ecf5267e8e3b
rolyPolyVole <yezhanting@gmail.com>
2024-10-11 00:23:48 +0100

    Change InventoryMoveItemEvent to InventoryClickEvent, add updateWoolColor()

--- src/main/java/org/esoteric_organisation/firework_wars_plugin/event/listeners/ItemOwnerChangeListener.java
+++ src/main/java/org/esoteric_organisation/firework_wars_plugin/event/listeners/ItemOwnerChangeListener.java
@@ -1,14 +1,14 @@
 package org.esoteric_organisation.firework_wars_plugin.event.listeners;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
-import org.bukkit.event.inventory.InventoryMoveItemEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 import org.esoteric_organisation.firework_wars_plugin.FireworkWarsPlugin;
+import org.esoteric_organisation.firework_wars_plugin.game.team.TeamPlayer;
 import org.esoteric_organisation.firework_wars_plugin.util.Keys;
 import org.esoteric_organisation.firework_wars_plugin.util.PersistentDataManager;
 
@@ -33,31 +33,41 @@ public class ItemOwnerChangeListener implements Listener {
             return;
         }
 
-        Bukkit.broadcastMessage("inside item pickup event");
-
+        updateWoolColor(item, player);
         checkAmmoOwner(item, player);
         updateItemLocale(item, player);
     }
 
     @EventHandler
-    public void onItemMoveInventory(InventoryMoveItemEvent event) {
-        ItemStack item = event.getItem();
+    public void onItemMoveInventory(InventoryClickEvent event) {
+        ItemStack item = event.getCurrentItem();
+        Player player = (Player) event.getWhoClicked();
 
-        Bukkit.broadcastMessage("inside item move inventory event");
-
-        if (!(event.getDestination().getHolder() instanceof Player player)) {
+        if (item == null || item.getType() == Material.AIR) {
             return;
         }
 
-        Bukkit.broadcastMessage("inside item move inventory event fully");
+        //todo: when halving a stack, make sure the stack remaining in the chest stays white
+        //todo: when putting the stuff back into the chest, turn the wool back to white
 
+        updateWoolColor(item, player);
         checkAmmoOwner(item, player);
         updateItemLocale(item, player);
     }
 
+    @SuppressWarnings("deprecation")
+    private void updateWoolColor(ItemStack item, Player player) {
+        if ("wool".equals(pdcManager.getStringValue(item.getItemMeta(), Keys.CUSTOM_ITEM_ID))) {
+            TeamPlayer teamPlayer = TeamPlayer.from(player);
+
+            if (teamPlayer != null) {
+                item.setType(teamPlayer.getTeam().getWoolMaterial());
+            }
+        }
+    }
+
     private void checkAmmoOwner(ItemStack item, Player player) {
         if (pdcManager.hasKey(item.getItemMeta(), Keys.AMMO_OWNER_UUID)) {
-            Bukkit.broadcastMessage("identified as ammo");
             item.editMeta(meta -> pdcManager.setUUIDValue(meta, Keys.AMMO_OWNER_UUID, player.getUniqueId()));
         }
     }
