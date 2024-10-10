e7e2673b8f234e124c4736f0d5b9ec566053f5dc
rolyPolyVole <yezhanting@gmail.com>
2024-10-10 17:54:47 +0100

    Revamp class, added method to update team lines with a team as a param

--- src/main/java/org/esoteric_organisation/firework_wars_plugin/scoreboard/wrapper/FireworkWarsScoreboard.java
+++ src/main/java/org/esoteric_organisation/firework_wars_plugin/scoreboard/wrapper/FireworkWarsScoreboard.java
@@ -1,10 +1,12 @@
 package org.esoteric_organisation.firework_wars_plugin.scoreboard.wrapper;
 
 import net.kyori.adventure.text.Component;
+import org.esoteric_organisation.firework_wars_plugin.game.team.FireworkWarsTeam;
 import org.esoteric_organisation.firework_wars_plugin.scoreboard.api.FastBoard;
 import org.esoteric_organisation.firework_wars_plugin.util.Pair;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,8 +17,8 @@ public class FireworkWarsScoreboard {
     private final List<Component> placeholderLines;
     private final List<Component> lines;
 
-    private final List<Component> teamPlaceholderLines;
-    private final List<Component> teamLines;
+    private final Map<FireworkWarsTeam, Component> teamPlaceholderLines;
+    private final Map<FireworkWarsTeam, Component> teamLines;
 
     private Component endgameLine;
     private boolean includeSecondEventLine;
@@ -33,11 +35,11 @@ public class FireworkWarsScoreboard {
         return lines;
     }
 
-    public List<Component> getTeamPlaceholderLines() {
+    public Map<FireworkWarsTeam, Component> getTeamPlaceholderLines() {
         return teamPlaceholderLines;
     }
 
-    public List<Component> getTeamLines() {
+    public Map<FireworkWarsTeam, Component> getTeamLines() {
         return teamLines;
     }
 
@@ -49,34 +51,20 @@ public class FireworkWarsScoreboard {
         this.includeSecondEventLine = includeSecondEventLine;
     }
 
-    public FireworkWarsScoreboard(FastBoard fastBoard) {
+    public FireworkWarsScoreboard(FastBoard fastBoard, Map<FireworkWarsTeam, Component> teamLines) {
         this.fastBoard = fastBoard;
 
-        this.placeholderLines = getLines(fastBoard);
+        this.placeholderLines = fastBoard.getLines();
         this.lines = new ArrayList<>(placeholderLines);
 
-        this.teamPlaceholderLines = getTeamLines(fastBoard);
-        this.teamLines = new ArrayList<>(teamPlaceholderLines);
-    }
-
-    private List<Component> getLines(FastBoard fastBoard) {
-        List<Component> lines = fastBoard.getLines();
-        List<Component> result = new ArrayList<>();
-
-        result.addAll(lines.subList(0, lines.indexOf(Component.empty()) + 1));
-        result.addAll(lines.subList(lines.lastIndexOf(Component.empty()), lines.size()));
-        return result;
-    }
-
-    private List<Component> getTeamLines(FastBoard fastBoard) {
-        List<Component> lines = fastBoard.getLines();
-        return lines.subList(lines.indexOf(Component.empty()) + 1, lines.lastIndexOf(Component.empty()));
+        this.teamPlaceholderLines = teamLines;
+        this.teamLines = new HashMap<>(teamLines);
     }
 
     private List<Component> combineLines() {
         List<Component> result = new ArrayList<>();
         result.addAll(lines.subList(0, lines.indexOf(Component.empty()) + 1));
-        result.addAll(teamLines);
+        result.addAll(teamLines.values());
         result.addAll(lines.subList(lines.lastIndexOf(Component.empty()), lines.size()));
 
         if (includeSecondEventLine) {
@@ -103,21 +91,21 @@ public class FireworkWarsScoreboard {
         return setLine(index, line);
     }
 
-    public FireworkWarsScoreboard setTeamLine(int index, Component line) {
-        teamLines.set(index, line);
+    public FireworkWarsScoreboard setTeamLine(FireworkWarsTeam team, Component line) {
+        teamLines.put(team, line);
         return this;
     }
 
     @SafeVarargs
-    public final FireworkWarsScoreboard updateTeamLine(int index, Map.Entry<String, String>... replacements) {
-        Component line = teamPlaceholderLines.get(index);
+    public final FireworkWarsScoreboard updateTeamLine(FireworkWarsTeam team, Pair<String, String>... replacements) {
+        Component line = teamPlaceholderLines.get(team);
 
-        for (Map.Entry<String, String> replacement : replacements) {
+        for (Pair<String, String> replacement : replacements) {
             line = line.replaceText(config ->
-                config.match(replacement.getKey()).replacement(replacement.getValue()));
+                config.match(replacement.getLeft()).replacement(replacement.getRight()));
         }
 
-        return setTeamLine(index, line);
+        return setTeamLine(team, line);
     }
 
     public void update() {
