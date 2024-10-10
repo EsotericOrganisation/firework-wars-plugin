b8786ecd4b90a995523b7257e17b8cacd3822604
rolyPolyVole <yezhanting@gmail.com>
2024-10-10 17:52:14 +0100

    Add events to manage item owners

---
+++ src/main/java/org/esoteric_organisation/firework_wars_plugin/event/listeners/ItemOwnerChangeListener.java
@@ -0,0 +1,79 @@
+package org.esoteric_organisation.firework_wars_plugin.event.listeners;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.inventory.InventoryMoveItemEvent;
+import org.bukkit.inventory.ItemStack;
+import org.esoteric_organisation.firework_wars_plugin.FireworkWarsPlugin;
+import org.esoteric_organisation.firework_wars_plugin.util.Keys;
+import org.esoteric_organisation.firework_wars_plugin.util.PersistentDataManager;
+
+public class ItemOwnerChangeListener implements Listener {
+    private final FireworkWarsPlugin plugin;
+    private final PersistentDataManager pdcManager;
+
+    public ItemOwnerChangeListener(FireworkWarsPlugin plugin) {
+        this.plugin = plugin;
+        this.pdcManager = plugin.getPdcManager();
+    }
+
+    public void register() {
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+    @EventHandler
+    public void onItemPickup(EntityPickupItemEvent event) {
+        ItemStack item = event.getItem().getItemStack();
+
+        if (!(event.getEntity() instanceof Player player)) {
+            return;
+        }
+
+        Bukkit.broadcastMessage("inside item pickup event");
+
+        checkAmmoOwner(item, player);
+        updateItemLocale(item, player);
+    }
+
+    @EventHandler
+    public void onItemMoveInventory(InventoryMoveItemEvent event) {
+        ItemStack item = event.getItem();
+
+        if (!(event.getDestination().getHolder() instanceof Player player)) {
+            return;
+        }
+
+        Bukkit.broadcastMessage("inside item move inventory event");
+
+        checkAmmoOwner(item, player);
+        updateItemLocale(item, player);
+    }
+
+    private void checkAmmoOwner(ItemStack item, Player player) {
+        if (pdcManager.hasKey(item.getItemMeta(), Keys.AMMO_OWNER_UUID)) {
+            Bukkit.broadcastMessage("identified as ammo");
+            plugin.runTaskLater(() -> pdcManager.setUUIDValue(
+                item.getItemMeta(), Keys.AMMO_OWNER_UUID, player.getUniqueId()), 1L);
+        }
+    }
+
+    private void updateItemLocale(ItemStack item, Player player) {
+        if (pdcManager.hasKey(item.getItemMeta(), Keys.CUSTOM_ITEM_ID)) {
+            if (item.getType() == Material.CROSSBOW) {
+                return; // Easter egg
+            }
+
+            String itemId = pdcManager.getStringValue(item.getItemMeta(), Keys.CUSTOM_ITEM_ID);
+            ItemStack newItem = plugin.getCustomItemManager().getItem(itemId).getItem(player);
+
+            item.editMeta(meta -> {
+                meta.displayName(newItem.getItemMeta().displayName());
+                meta.lore(newItem.getItemMeta().lore());
+            });
+        }
+    }
+}
