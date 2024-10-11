2089c00cf227e5f74e056ab9a363d9aa9701f906
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-10-11 19:56:21 +0100

    Add more permission checks

--- src/main/java/org/esoteric/minecraft/plugins/fireworkwars/commands/HealCommand.java
+++ src/main/java/org/esoteric/minecraft/plugins/fireworkwars/commands/HealCommand.java
@@ -3,11 +3,14 @@ package org.esoteric.minecraft.plugins.fireworkwars.commands;
 import dev.jorel.commandapi.CommandAPICommand;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.permissions.ServerOperator;
 import org.esoteric.minecraft.plugins.fireworkwars.FireworkWarsPlugin;
 
 public class HealCommand extends CommandAPICommand {
     public HealCommand(FireworkWarsPlugin plugin) {
         super("heal");
+
+        setRequirements(ServerOperator::isOp);
 
         executesPlayer((player, args) -> {
             player.setHealth(20.0D);
