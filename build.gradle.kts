f170c8c728e4e691e92535d27c682745e4e161a2
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-10-11 18:50:57 +0100

    Replace plugin suffix in name

--- build.gradle.kts
+++ build.gradle.kts
@@ -96,7 +96,10 @@ tasks {
   }
 }
 
+val projectName = providers.gradleProperty("projectName").get()
+
 paperPluginYaml {
+  name = projectName.replace(Regex(" Plugin$"), "")
   authors = projectAuthors
 
   main = projectGroupString + groupStringSeparator + pascalCase(projectNameString)
