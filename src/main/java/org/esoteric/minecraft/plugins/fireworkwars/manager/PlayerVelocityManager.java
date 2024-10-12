3a401b18b2bd704d86912e2e45384f33043a4ed9
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-12 22:02:12 +0100

    Create player velocity manager

---
+++ src/main/java/org/esoteric/minecraft/plugins/fireworkwars/manager/PlayerVelocityManager.java
@@ -0,0 +1,37 @@
+package org.esoteric.minecraft.plugins.fireworkwars.manager;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
+import org.esoteric.minecraft.plugins.fireworkwars.FireworkWarsPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class PlayerVelocityManager {
+
+    private final Map<UUID, Vector> playerVelocityMap = new HashMap<>();
+    private final Map<UUID, Vector> playerPreviousPositionMap = new HashMap<>();
+
+    public PlayerVelocityManager(FireworkWarsPlugin plugin) {
+        Bukkit.getScheduler().runTaskTimer(plugin, () -> {
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                Vector previousPosition = playerPreviousPositionMap.get(player.getUniqueId());
+
+                if (previousPosition == null) {
+                    playerPreviousPositionMap.put(player.getUniqueId(), player.getLocation().toVector());
+                    continue;
+                }
+
+                Vector velocity = player.getLocation().toVector().subtract(previousPosition);
+                playerVelocityMap.put(player.getUniqueId(), velocity);
+            }
+        }, 0L, 1L);
+    }
+
+    public Vector getPlayerVelocity(@NotNull Player player) {
+        return playerVelocityMap.get(player.getUniqueId());
+    }
+}
