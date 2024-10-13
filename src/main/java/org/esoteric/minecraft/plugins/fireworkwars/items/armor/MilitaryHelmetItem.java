82259ff21d7cd38c9292b853d21b9d7902fb2224
rolyPolyVole <yezhanting@gmail.com>
2024-10-13 15:53:15 +0100

    Add attribute modifier

--- src/main/java/org/esoteric/minecraft/plugins/fireworkwars/items/armor/MilitaryHelmetItem.java
+++ src/main/java/org/esoteric/minecraft/plugins/fireworkwars/items/armor/MilitaryHelmetItem.java
@@ -1,14 +1,18 @@
 package org.esoteric.minecraft.plugins.fireworkwars.items.armor;
 
 import org.bukkit.Material;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.EquipmentSlotGroup;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ArmorMeta;
 import org.esoteric.minecraft.plugins.fireworkwars.FireworkWarsPlugin;
 import org.esoteric.minecraft.plugins.fireworkwars.items.AbstractItem;
 import org.esoteric.minecraft.plugins.fireworkwars.language.Message;
 import org.esoteric.minecraft.plugins.fireworkwars.util.ItemBuilder;
+import org.esoteric.minecraft.plugins.fireworkwars.util.Keys;
 
 public class MilitaryHelmetItem extends AbstractItem<ArmorMeta> {
     public MilitaryHelmetItem(FireworkWarsPlugin plugin) {
@@ -26,9 +30,18 @@ public class MilitaryHelmetItem extends AbstractItem<ArmorMeta> {
     }
 
     @Override
+    @SuppressWarnings("UnstableApiUsage")
     protected void modifyMeta(ArmorMeta meta) {
         super.modifyMeta(meta);
         meta.addEnchant(Enchantment.BLAST_PROTECTION, 1, true);
+
+        AttributeModifier modifier = new AttributeModifier(
+            Keys.MILITARY_HELMET_ATTRIBUTE_MOD,
+            2,
+            AttributeModifier.Operation.ADD_NUMBER,
+            EquipmentSlotGroup.HEAD);
+
+        meta.addAttributeModifier(Attribute.GENERIC_ARMOR, modifier);
     }
 
     @Override
