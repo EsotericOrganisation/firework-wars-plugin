bd6c52a9168332f68caaacde566c9a0e25959632
rolyPolyVole <yezhanting@gmail.com>
2024-10-13 15:53:00 +0100

    Add attribute modifier

--- src/main/java/org/esoteric/minecraft/plugins/fireworkwars/items/armor/HeavyArmorItem.java
+++ src/main/java/org/esoteric/minecraft/plugins/fireworkwars/items/armor/HeavyArmorItem.java
@@ -2,8 +2,11 @@ package org.esoteric.minecraft.plugins.fireworkwars.items.armor;
 
 import org.bukkit.Color;
 import org.bukkit.Material;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.EquipmentSlotGroup;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.esoteric.minecraft.plugins.fireworkwars.FireworkWarsPlugin;
@@ -11,10 +14,11 @@ import org.esoteric.minecraft.plugins.fireworkwars.game.team.TeamPlayer;
 import org.esoteric.minecraft.plugins.fireworkwars.items.AbstractItem;
 import org.esoteric.minecraft.plugins.fireworkwars.language.Message;
 import org.esoteric.minecraft.plugins.fireworkwars.util.ItemBuilder;
+import org.esoteric.minecraft.plugins.fireworkwars.util.Keys;
 
 public class HeavyArmorItem extends AbstractItem<LeatherArmorMeta> {
     public HeavyArmorItem(FireworkWarsPlugin plugin) {
-        super(plugin, "heavy_armor", Material.DIAMOND_CHESTPLATE, 3, 25);
+        super(plugin, "heavy_armor", Material.LEATHER_CHESTPLATE, 3, 25);
     }
 
     @Override
@@ -36,10 +40,19 @@ public class HeavyArmorItem extends AbstractItem<LeatherArmorMeta> {
             .build();
     }
 
+    @SuppressWarnings("UnstableApiUsage")
     private void modifyArmorMeta(LeatherArmorMeta meta, Color color) {
         super.modifyMeta(meta);
         meta.setColor(color);
         meta.addEnchant(Enchantment.BLAST_PROTECTION, 1, true);
+
+        AttributeModifier modifier = new AttributeModifier(
+            Keys.HEAVY_ARMOR_ATTRIBUTE_MOD,
+            5,
+            AttributeModifier.Operation.ADD_NUMBER,
+            EquipmentSlotGroup.CHEST);
+
+        meta.addAttributeModifier(Attribute.GENERIC_ARMOR, modifier);
     }
 
     @Override
