6aa696554611da4e8b39dce6d15559fa333314e7
rolyPolyVole <yezhanting@gmail.com>
2024-10-13 12:58:43 +0100

    Cleanup code

--- src/main/java/org/esoteric/minecraft/plugins/fireworkwars/commands/ArenaCommand.java
+++ src/main/java/org/esoteric/minecraft/plugins/fireworkwars/commands/ArenaCommand.java
@@ -1,8 +1,13 @@
 package org.esoteric.minecraft.plugins.fireworkwars.commands;
 
 import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.arguments.Argument;
 import dev.jorel.commandapi.arguments.ArgumentSuggestions;
 import dev.jorel.commandapi.arguments.IntegerArgument;
+import dev.jorel.commandapi.commandsenders.BukkitPlayer;
+import dev.jorel.commandapi.executors.ExecutionInfo;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.esoteric.minecraft.plugins.fireworkwars.FireworkWarsPlugin;
 import org.esoteric.minecraft.plugins.fireworkwars.arena.json.structure.Arena;
 import org.esoteric.minecraft.plugins.fireworkwars.arena.manager.ArenaManager;
@@ -16,68 +21,85 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class ArenaCommand extends CommandAPICommand {
+    private final String arenaNumberNodeName = "arena-number";
+
+    private final FireworkWarsPlugin plugin;
+
+    private final ArenaManager arenaManager;
+    private final GameManager gameManager;
 
     public ArenaCommand(FireworkWarsPlugin plugin) {
         super("arena");
 
-        ArenaManager arenaManager = plugin.getArenaManager();
-        List<Arena> arenas = arenaManager.getArenas();
+        this.plugin = plugin;
 
-        GameManager gameManager = plugin.getGameManager();
-
-        String arenaNumberNodeName = "arena-number";
+        this.arenaManager = plugin.getArenaManager();
+        this.gameManager = plugin.getGameManager();
 
         withSubcommand(
-                new CommandAPICommand("join")
-                        .withArguments(new IntegerArgument(arenaNumberNodeName).includeSuggestions(ArgumentSuggestions.strings((suggestionsInfo) -> {
-                            List<String> suggestions = new ArrayList<>();
-
-                            Predicate<Arena> hasOngoingGame = gameManager::hasOngoingGame;
-                            Function<Integer, Integer> increment = i -> ++i;
-
-                            arenas.stream()
-                                    .filter(hasOngoingGame.negate())
-                                    .map(arenas::indexOf).map(increment)
-                                    .map(String::valueOf)
-                                    .forEach(suggestions::add);
-
-                            return suggestions.toArray(String[]::new);
-                        })))
-                        .executesPlayer((info) -> {
-                            if (!arenaManager.isLobby(info.sender().getWorld())) {
-                                plugin.getLanguageManager().sendMessage(Message.NOT_IN_LOBBY, info.sender());
-                                return;
-                            }
-
-                            Integer arenaNumber = (Integer) info.args().get(arenaNumberNodeName);
-
-                            if (arenaNumber == null || arenaNumber <= 0 || arenaNumber - 1 >= arenas.size()) {
-                                plugin.getLanguageManager().sendMessage(Message.INVALID_ARENA, info.sender());
-                                return;
-                            }
-
-                            Arena arena = arenas.get(arenaNumber - 1);
-                            FireworkWarsGame game = gameManager.getFireworkWarsGame(arena);
-
-                            if (game.isPlaying()) {
-                                plugin.getLanguageManager().sendMessage(Message.GAME_ALREADY_PLAYING, info.sender());
-                                return;
-                            }
-
-                            if (game.containsPlayer(info.sender())) {
-                                plugin.getLanguageManager().sendMessage(Message.GAME_ALREADY_CONTAINS_PLAYER, info.sender());
-                                return;
-                            }
-
-                            if (game.isResetting()) {
-                                plugin.getLanguageManager().sendMessage(Message.GAME_RELOADING, info.sender());
-                                return;
-                            }
-
-                            game.addPlayer(info.sender());
-                        })
-        );
+            new CommandAPICommand("join")
+                .withArguments(this.getArenaArguments())
+                .executesPlayer(this::onPlayerExecution));
 
         register(plugin);
+    }
+
+    private Argument<Integer> getArenaArguments() {
+        return new IntegerArgument(arenaNumberNodeName)
+            .includeSuggestions(ArgumentSuggestions.strings((suggestionsInfo) -> {
+                List<String> suggestions = new ArrayList<>();
+
+                Predicate<Arena> hasOngoingGame = gameManager::hasOngoingGame;
+                Function<Integer, Integer> increment = i -> ++i;
+
+                List<Arena> arenas = arenaManager.getArenas();
+
+                arenas.stream()
+                    .filter(hasOngoingGame.negate())
+                    .map(arenas::indexOf).map(increment)
+                    .map(String::valueOf)
+                    .forEach(suggestions::add);
+
+                return suggestions.toArray(String[]::new);
+        }));
+    }
+
+    private void onPlayerExecution(ExecutionInfo<Player, BukkitPlayer> info) {
+        Player player = info.sender();
+        Integer arenaNumber = (Integer) info.args().get(arenaNumberNodeName);
+
+        List<Arena> arenas = arenaManager.getArenas();
+
+        if (arenaNumber == null || arenaNumber <= 0 || arenaNumber - 1 >= arenas.size()) {
+            plugin.getLanguageManager().sendMessage(Message.INVALID_ARENA, player);
+            return;
+        }
+
+        Arena arena = arenas.get(arenaNumber - 1);
+        FireworkWarsGame game = gameManager.getFireworkWarsGame(arena);
+
+        World world = player.getWorld();
+
+        if (!arenaManager.isLobby(world) && !arenaManager.isArena(world)) {
+            plugin.getLanguageManager().sendMessage(Message.NOT_IN_LOBBY, player);
+            return;
+        }
+
+        if (game.isPlaying()) {
+            plugin.getLanguageManager().sendMessage(Message.GAME_ALREADY_PLAYING, player);
+            return;
+        }
+
+        if (game.containsPlayer(player)) {
+            plugin.getLanguageManager().sendMessage(Message.GAME_ALREADY_CONTAINS_PLAYER, player);
+            return;
+        }
+
+        if (game.isResetting()) {
+            plugin.getLanguageManager().sendMessage(Message.GAME_RELOADING, player);
+            return;
+        }
+
+        game.addPlayer(player);
     }
 }
