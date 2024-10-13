05af52dba318375599c34840437ac7f29fe4ffbf
rolyPolyVole <yezhanting@gmail.com>
2024-10-13 15:53:08 +0100

    Add attribute modifier namespaced keys

--- src/main/java/org/esoteric/minecraft/plugins/fireworkwars/util/Keys.java
+++ src/main/java/org/esoteric/minecraft/plugins/fireworkwars/util/Keys.java
@@ -4,11 +4,16 @@ import org.bukkit.NamespacedKey;
 
 public final class Keys {
     public static final NamespacedKey CUSTOM_ITEM_DATA = fromString("custom_item_data");
+
     public static final NamespacedKey CUSTOM_ITEM_ID = fromString("custom_item_id");
     public static final NamespacedKey AMMO_OWNER_UUID = fromString("ammo_owner_uuid");
     public static final NamespacedKey GUN_ACCEPTED_AMMO_ID = fromString("gun_accepted_ammo_id");
     public static final NamespacedKey PLAYER_COMPASS_ID = fromString("player_compass_id");
+
     public static final NamespacedKey EXPLOSIVE_ROCKET_ENTITY = fromString("explosive_rocket_key");
+
+    public static final NamespacedKey MILITARY_HELMET_ATTRIBUTE_MOD = fromString("military_helmet_attribute_mod");
+    public static final NamespacedKey HEAVY_ARMOR_ATTRIBUTE_MOD = fromString("heavy_armor_attribute_mod");
 
     private Keys() {}
 
