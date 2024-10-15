bbd3df48b711769b92688d72b8436a6f1c5780ae
rolyPolyVole <yezhanting@gmail.com>
2024-10-15 12:00:06 +0100

    Add maxChestValue percentage to item types

--- src/main/java/org/esoteric/minecraft/plugins/fireworkwars/items/ItemType.java
+++ src/main/java/org/esoteric/minecraft/plugins/fireworkwars/items/ItemType.java
@@ -1,11 +1,20 @@
 package org.esoteric.minecraft.plugins.fireworkwars.items;
 
 public enum ItemType {
-    ARMOR,
-    HEALING,
-    CONSUMABLE,
-    EXPLOSIVE,
-    GUN,
-    AMMO,
-    MISC
+    ARMOR(0.8D),
+    CONSUMABLE(0.6D),
+    EXPLOSIVE(0.3D),
+    GUN(1.0D),
+    AMMO(0.2D),
+    MISC(0.5D);
+
+    private final double maxChestValue;
+
+    ItemType(double maxChestValue) {
+        this.maxChestValue = maxChestValue;
+    }
+
+    public double getMaxChestPercentage() {
+        return maxChestValue;
+    }
 }
