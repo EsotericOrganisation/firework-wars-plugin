34cacaf3437faf7adb934ccace353cf06648ca26
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-17 13:35:16 +0100

    Disallow breaking blocks while not in a game

--- src/main/java/org/esoteric/minecraft/plugins/games/firework/wars/FireworkWarsPlugin.java
+++ src/main/java/org/esoteric/minecraft/plugins/games/firework/wars/FireworkWarsPlugin.java
@@ -7,6 +7,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
@@ -221,6 +222,17 @@ public final class FireworkWarsPlugin extends JavaPlugin implements Listener {
         if (!(event.getEntity() instanceof Player player)) {
            return;
         }
+
+        FireworkWarsGame game = gameManager.getFireworkWarsGame(player);
+
+        if (game == null) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onBlockBreak(@NotNull BlockBreakEvent event) {
+        Player player = event.getPlayer();
 
         FireworkWarsGame game = gameManager.getFireworkWarsGame(player);
 
